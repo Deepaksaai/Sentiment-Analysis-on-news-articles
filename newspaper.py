0df89b6860ac1044cae098e20f13b3a9d0c5f8b8
Deepak <deepak05042007@gmail.com>
2026-01-11 22:46:36 +0530

    added newspaper article fetching feature

--- newspaper.py
+++ newspaper.py
@@ -1,1 +1,13 @@
-print("hello")
+import requests
+#fetch top headlines from the US
+url = "https://newsapi.org/v2/top-headlines"
+params = {
+    'country': 'us',
+    'apiKey': '24eb538ed65d4fe29a81ec519e82fb1c' 
+}
+response = requests.get(url, params=params)
+data = response.json()
+articles = data.get('articles', [])
+for article in articles:
+    print(f"Title(usa): {article['title']}")
+    print(f"Description: {article['description']}\n")
